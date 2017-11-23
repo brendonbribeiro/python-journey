4a7d14b675cea0bebf0f5649ad00715e332550da
Brendon Barreto <brendondominic@gmail.com>
2017-11-22 23:40:52 -0200

    len and conditionals

---
+++ language_basics/conditionals.py
@@ -0,0 +1,10 @@
+a = 6
+
+if a < 5:
+    print("less")
+elif(a == 5):
+    print("equals")
+else:
+    print("greater")
+
+print("Cool" if a <=5 else "Boring")
---
+++ language_basics/str_length.py
@@ -0,0 +1,4 @@
+def str_length(text):
+    return len(text)
+
+print(str_length("FooBar"))
