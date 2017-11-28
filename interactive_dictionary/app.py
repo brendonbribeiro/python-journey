81330b368e35a36f4dbc74051af0427ba95e370f
Brendon Barreto <brendondominic@gmail.com>
2017-11-28 00:15:19 -0200

    interactive dictionary completed

--- interactive_dictionary/app.py
+++ interactive_dictionary/app.py
@@ -1,6 +1,36 @@
 import json
-
+from difflib import get_close_matches
+#Calculate proximity ratio
+#from difflib import SequenceMatcher
+#print(SequenceMatcher(a="rain", b="rainn").ratio())
 dict = json.load(open("data.json"))
 
-dict["rain"]
-print(dict["rain"])
+def get_right_word(word):
+    word = word.lower()
+    if word in dict:
+        return (True, word)
+    elif word.title() in dict:
+        return (True, word.title())
+    else:
+        close_matches = get_close_matches(word, dict.keys(), 1, 0.8)
+        if len(close_matches) > 0:
+            right_word =  close_matches[0]
+            user_option = input("Did you mean %s instead? Enter Y if yes, and N if no: " % right_word)
+            if user_option.upper() == "Y":
+                return (True, right_word)
+            else:
+                return (False, word)
+        else:
+            return (False, word)
+
+def translate(word):
+    meanings = dict[word]
+    formatted = "\n".join(meanings)
+    return formatted
+
+word = input("Enter word: ")
+rg = get_right_word(word)
+if rg[0]:
+    print(translate(rg[1]))
+else:
+    print("The word %s doesn't exist. Please double check it" % word)
